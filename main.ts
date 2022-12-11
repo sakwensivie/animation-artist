909248cf07109d89cff4bf05df5a0b25e572089f
sakwensivie <63986400+sakwensivie@users.noreply.github.com>
2022-12-11 00:51:18 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,67 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
 	
 })
+while (true) {
+    basic.showIcon(IconNames.Duck)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        # # # . .
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+}
